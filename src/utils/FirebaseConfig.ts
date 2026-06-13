930c012ae2fc42a1d84c485c658aa01f53654033
Fazle Rabbi <mmrabbi625442@gmail.com>
2026-06-14 01:29:50 +0600

    hiden the firebase credentials

--- src/utils/FirebaseConfig.ts
+++ src/utils/FirebaseConfig.ts
@@ -1,22 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxod-eup82Oy_Od04YLHs7iOQdFGBmEHU",
-  authDomain: "ruet-cse-24.firebaseapp.com",
-  projectId: "ruet-cse-24",
-  storageBucket: "ruet-cse-24.firebasestorage.app",
-  messagingSenderId: "45659772253",
-  appId: "1:45659772253:web:963ee9de7794ee482d84b4",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
   measurementId: "G-B8107S52H7",
 };
 
 const app = initializeApp(firebaseConfig);
-
-// initializeAppCheck(app, {
-//   provider: new ReCaptchaV3Provider("6LcO7aYsAAAAAP-IPs5gALP0XxwlG-KqgLUsgb_k"),
-//   isTokenAutoRefreshEnabled: true,
-// });
 
 export const db = getFirestore(app);
